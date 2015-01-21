b854daca8c0c8811ad8925a971f0714ba1bb206f
PrevaultNoel <noel.prevault@gmail.com>
2015-01-21 14:45:57 +0100

    Creation ViewActuController

---
+++ AppliIUTSDdeLorraine/ViewActuController.h
@@ -0,0 +1,13 @@
+//
+//  ViewActuController.h
+//  AppliIUTSDdeLorraine
+//
+//  Created by Noël Prévault on 21/01/2015.
+//  Copyright (c) 2015 Damien Bannerot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ViewActuController 
+
+@end
---
+++ AppliIUTSDdeLorraine/ViewActuController.m
@@ -0,0 +1,13 @@
+//
+//  ViewActuController.m
+//  AppliIUTSDdeLorraine
+//
+//  Created by Noël Prévault on 21/01/2015.
+//  Copyright (c) 2015 Damien Bannerot. All rights reserved.
+//
+
+#import "ViewActuController.h"
+
+@implementation ViewActuController
+
+@end
